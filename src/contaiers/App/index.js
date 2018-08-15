5a8959ae5253fab3461544c738ff7b53e8f3e5ff
longhaoz <zhanglonghao@neusoft.com>
2018-08-15 09:13:03 +0800

    update

---
+++ src/contaiers/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
